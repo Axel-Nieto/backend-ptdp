7c9b95d3e1da2bf5bffc0a35f9eb57a6d32afdb0
Axel Nieto <axelnieto616@gmail.com>
2023-04-03 16:29:00 -0300

    primer commit

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using backend_ptdp.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +30,8 @@ namespace backend_ptdp
         {
 
             services.AddControllers();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Conexion")));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "backend_ptdp", Version = "v1" });
